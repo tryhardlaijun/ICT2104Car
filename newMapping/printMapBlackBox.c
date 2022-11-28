cc2cec86b5b1eaa22abe9fa66b08c3e69a65b195
Tryhardlaijun <wenjun456@gmail.com>
2022-11-28 14:09:26 +0800

    added blackbox

---
+++ newMapping/printMapBlackBox.c
@@ -0,0 +1,27 @@
+
+#include "coordinate.h"
+#include "printMapBlackBox.h"
+
+void testWithNegXandYSensorArray(int *buff, int count) {
+    int testSensorArray[] = {0b001, 0b001, 0b010, 0b101,
+                            0b100, 0b001, 0b100, 
+                            0b001, 0b001, 0b001, 0b100,
+                            0b001, 0b101,
+                            0b000,
+                            0b001, 0b100, 
+                            0b001, 0b001, 0b100,};
+                            
+    for (int i=0;i<count;i++) {
+        buff[i] = testSensorArray[i];
+    }
+}
+
+coordinate testWithNegXandYStartCoord() {
+    coordinate testStart = {0,0,0,0};
+    return testStart;
+}
+
+coordinate testWithNegXandYEndCoord() {
+    coordinate testEnd = {1,0,0,0};
+    return testEnd;
+}
